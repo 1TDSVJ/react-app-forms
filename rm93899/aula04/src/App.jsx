62c2a5e0512958cefd9d65af68dc23e6eeaf390a
EmersonGald1n0 <rm93899@fiap.com.br>
2022-08-25 09:52:27 -0300

    App.jsx att

--- rm93899/aula04/src/App.jsx
+++ rm93899/aula04/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Menu from './components/menu/Menu'
 import MinhasRotas from './components/routes/MinhasRotas'
-
+destru
 export default function App() {
   return (
     <div>
