50a080866b4a2fa2c4b8bae530fc00e3a52abbb6
DiogoKahn <rm92928@fiap.com.br>
2022-08-25 08:16:53 -0300

    Criação do boyler plate

---
+++ rm92928/aula04/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.createRoot("CONTEÚDO", document.getElementById('root'));
