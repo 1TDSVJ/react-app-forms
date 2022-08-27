aa29af7087273b64512ae0f818452316528522a4
MateusLem <rm93480@fiap.com.br>
2022-08-27 13:58:50 -0300

    criado

---
+++ rm93480/aula 04/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NovoValor from "./components/novoValor/NovoValor.jsx"
+
+
+export default function App() {
+  return(
+    <div>
+        <h1>APP</h1>
+        <div>
+            <NovoValor/>
+        </div>
+    </div>
+  )
+}
